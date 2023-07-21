a78a8cbf48a8bb90b318b80c1edcf7c52bb84351
dkayabasi <kayabasiduygu@gmail.com>
2023-07-21 18:35:46 +0300

    SEction2

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+using Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+           
+
+        }
+
+       [HttpGet] //api/activities
+       public async Task<ActionResult<List<Activity>>> GetActivities()
+       {
+        return await _context.Activities.ToListAsync();
+        
+       }
+
+        [HttpGet("{id}")] //api/activities/kdjfkjfk
+       public async Task<ActionResult<Activity>> GetActivity(Guid id)
+       {
+        return await _context.Activities.FindAsync(id);
+        
+       }
+
+    }
+}
